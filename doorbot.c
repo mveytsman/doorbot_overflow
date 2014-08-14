e22dc03a8d6d8036898e94cab569cc3deeb933e4
Paul Tagliamonte <tag@pault.ag>
2014-08-14 14:53:39 -0400

    Add a flush for all puts.

--- doorbot.c
+++ doorbot.c
@@ -4,27 +4,28 @@
 char* SECRET_HACKERSCHOOL_PASSWORD = "hunter1";
 
 int check_password() {
-    int success = 0;
+    // int success = 0;
     char password_guess[32];
     puts("Enter the password to open the door");
+    fflush(stdout);
     scanf("%[^\n]", password_guess);
-    if (strcmp(password_guess, SECRET_HACKERSCHOOL_PASSWORD) == 0) {
-        success = 1;
-    }
-    return success;
+    return (strcmp(password_guess, SECRET_HACKERSCHOOL_PASSWORD) == 0);
 }
 
 void open_door() {
     puts("Opening Hacker School Door.");
     //TODO learn how to solder;
+    fflush(stdout);
 }
 
 int main(int argc, char **argv) {
     puts("Hi there!\nWelcome to Hacker School.");
+    fflush(stdout);
     if (check_password()) {
         open_door();
     } else {
         puts("I can't let you do that Dave");
+        fflush(stdout);
     }
     return 0;
 }
